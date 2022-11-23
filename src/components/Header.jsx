aef440f70abdae8e236909f92dd6bb10056f8878
shahank <crazykavlogs@gmail.com>
2022-11-24 01:20:39 +0530

    Created the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,34 @@
 import React from "react";
 
-function Header() {
+export default function Header() {
   return (
     <>
-      <nav className="relative container mx-auto p-6">
-        <div className="flex items-center justify-between">
+      <nav className="container mx-auto py-2.5 px-4 mt-2 rounded-xl bg-gray-900 w-full z-20 border-gray-600">
+        <div className="container flex flex-wrap mx-auto items-center justify-between">
+
           <div className="pt-2">
-            <h1>dwitter</h1>
+            <span className="text-gray-100 self-center ml-4 text-xl font-semibold">dwitter</span>
           </div>
 
-          <div className="hidden md:flex space-x-6">
-            <a href="#" className="hover:text-orange-600">About</a>
-            <a href="#" className="hover:text-orange-600">Profile</a>
+          <div className="flex items-center w-auto">
+            <ul className="flex flex-row space-x-8 p-4 mt-4 font-medium">
+              <li>
+                <a href="#" className="block pl-2 px-3 text-gray-500 hover:text-gray-100">About</a>
+              </li>
+              <li>
+                <a href="#" className="block pl-2 px-3 text-gray-500 hover:text-gray-100">Profile</a>
+              </li>
+            </ul>
           </div>
+
+          <form>
+            <div className="relative">
+              <input type="search" id="search" className="w-auto p-4 py-3 text-sm border border-gray-300 rounded-lg bg-gray-700 focus:border-blue-500 text-white" placeholder="Query..." required/>
+              <button type="submit" className="text-white bg-blue-600 hover:bg-blue-700 px-2 py-3 ml-2 rounded-lg">Search!</button>
+            </div>
+          </form>
         </div>
       </nav>
     </>
   );
 }
-
-export default Header;
